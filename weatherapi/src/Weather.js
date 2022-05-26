23ac5a4bb8f6590390f926302cf4eeeac045de34
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-05-26 16:15:29 +0300

    Added Weather.js and Images

--- weatherapi/src/Weather.js
+++ weatherapi/src/Weather.js
@@ -12,7 +12,7 @@ export default class Weather extends Component {
     }
   render() {
     return (
-      <div className= "my-3">
+      <div >
         <h1> On {this.state.day}, there is Raining</h1>
         <h1> On {this.state.day}, there was Sunny</h1>
       </div>
